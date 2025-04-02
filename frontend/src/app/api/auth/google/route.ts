ddc63767741b305cdb77e0d1c43d307005c8e6e5
Louis Volant <volantlouis@gmail.com>
2025-04-02 19:10:00 +0100

    Google Oauth code cleaning

--- frontend/src/app/api/auth/google/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET() {
-  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&scope=email profile`;
-  return NextResponse.redirect(url);
-}
--- frontend/src/app/api/check-auth/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
-
-export async function GET() {
-  const session = cookies().get("session")?.value;
-  return NextResponse.json({ isAuthenticated: !!session });
-}
