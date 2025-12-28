784566171089a93a27c664dfa09dc0171bb7fce0
Louis Volant <volantlouis@gmail.com>
2025-12-28 11:38:47 +0100

    Updated footer links

--- frontend/src/app/links.ts
+++ frontend/src/app/links.ts
@@ -14,7 +14,9 @@ export const externalLinks: LinkItem[] = [
   { label: 'Personal Page', href: 'https://www.louisvolant.com' },
   { label: 'MP3 Tool', href: 'https://mp3-tool.louisvolant.com' },
   { label: 'Skipass Checker', href: 'https://skipass-earlybird-checker.louisvolant.com' },
-  { label: 'Rain Under The Cloud', href: 'https://rainunderthe.cloud' },
+  { label: 'Sun Over The Cloud', href: 'https://sunoverthe.cloud' },
   { label: 'Build My CV', href: 'https://buildmycv.net' },
   { label: 'My 20 years old blog', href: 'https://www.abricocotier.fr' },
+  { label: 'Currency Converter', href: 'https://currency-converter-pwa-js.vercel.app/' },
+  { label: 'Whois', href: 'https://whois-js.vercel.app/' },
 ];
